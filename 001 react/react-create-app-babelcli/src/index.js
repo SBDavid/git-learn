d181b75aa43ce04a7722298248fa7d2c04d518c9
davidtangjw <davidtangjw@pptv.com>
2017-06-29 17:36:50 +0800

    使用babel-cli创建新的页面

---
+++ 001 react/react-create-app-babelcli/src/index.js
@@ -0,0 +1,21 @@
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+window.onload = function() {
+    ReactDOM.render(
+    <ShoppingList name="David"/>,
+    document.getElementById('root')
+    );
+}
