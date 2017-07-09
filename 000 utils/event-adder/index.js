98be7be7d2471094f5dc465375193bf142034e7b
SBDavid <billl89@126.com>
2017-07-09 16:40:19 +0800

    事件管理包

--- 000 utils/event-adder/index.js
+++ 000 utils/event-adder/index.js
@@ -1,45 +1,21 @@
-(function (global) {
-
-    //
-    // export if necessary
-    //
-
-    if (typeof exports !== 'undefined' && exports) {
-        // node.js
-        exports.EventUtil = EventUtil;
-    }
-    else {
-        // AMD
-        if (typeof define == 'function' && define.amd) {
-            define(function () {
-                return EventUtil;
-            });
+var EventUtil = {
+    addHandler: function (element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
         }
-        else {
-            // in browser add to global
-            global['EventUtil'] = EventUtil;
+    },
+    removeHandler: function (element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
         }
     }
-
-    var EventUtil = {
-        addHandler: function (element, type, handler) {
-            if (element.addEventListener) {
-                element.addEventListener(type, handler, false);
-            } else if (element.attachEvent) {
-                element.attachEvent("on" + type, handler);
-            } else {
-                element["on" + type] = handler;
-            }
-        },
-        removeHandler: function (element, type, handler) {
-            if (element.removeEventListener) {
-                element.removeEventListener(type, handler, false);
-            } else if (element.detachEvent) {
-                element.detachEvent("on" + type, handler);
-            } else {
-                element["on" + type] = null;
-            }
-        }
-    };
-
-})(typeof window != 'undefined' ? window : typeof global != 'undefined' ? global : typeof self != 'undefined' ? self : this);
+};
+exports.EventUtil = EventUtil;
