432bb8fb68ff3a6c238dfbc8f6a42940ef439d26
davidtangjw <davidtangjw@pptv.com>
2017-07-17 18:26:55 +0800

    update

---
+++ 010 jquery/002 Deferred/js/002_donefail.js
@@ -0,0 +1,19 @@
+function runAsync(){
+    var def = $.Deferred();
+    //做一些异步操作
+    setTimeout(function(){
+        console.log('执行完成');
+
+        def.resolve('resolve');
+    }, 500);
+    return def.promise();
+}
+
+
+runAsync()
+.done(function(data){
+    console.log(data)
+})
+.fail(function(){
+    console.log('执行失败');
+});;
