79f8457e29c15fb667bf4a35a68206a9873c07c0
davidtangjw <davidtangjw@pptv.com>
2017-08-04 16:52:15 +0800

    update

--- 015 canvens/js/index.js
+++ 015 canvens/js/index.js
@@ -3,16 +3,27 @@ window.onload = function() {
 
     var ctx = canvas.getContext('2d');
 
-    // 文字渲染
-    ctx.font = "24px serif";
-    ctx.fillText("Helloworld", 0, 100);
+    
     // 文字测量
-    var text = ctx.measureText("Helloworld唐");
+    ctx.font = "24px serif";
+    ctx.textBaseline = 'bottom';
+    var text = ctx.measureText("Hello world");
     console.info(text);
 
     // 绘制圆形
     ctx.fillStyle = "rgba(0,0,0,0.3)";
     ctx.beginPath();
-    ctx.arc(50, 200, 30, 0, Math.PI * 2, true);
+    ctx.arc(50, 200, 12, -Math.PI / 2, Math.PI / 2, true);
     ctx.fill();
+
+    // 绘制矩形
+    ctx.fillRect(50,188,132,24);
+
+    // 绘制圆形
+    ctx.beginPath();
+    ctx.arc(182, 200, 12, Math.PI / 2, Math.PI * 1.5, true);
+    ctx.fill();
+
+    // 文字渲染
+    ctx.fillText("Hello world", 50, 212);
 }
