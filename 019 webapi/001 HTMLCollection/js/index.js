de750f967fcef4d35457f4532fb8305c17961401
SBDavid <billl89@126.com>
2017-08-16 21:42:14 +0800

    update

--- 019 webapi/001 HTMLCollection/js/index.js
+++ 019 webapi/001 HTMLCollection/js/index.js
@@ -9,6 +9,9 @@ window.onload = function() {
     var root = document.getElementById('root');
 
     // displayProp(root);
+
+    console.info('document的类型', Object.prototype.toString.call(document));
+
     console.info('Div节点的类型', Object.prototype.toString.call(root));
     console.info('Div.children节点的类型', Object.prototype.toString.call(root.children), root.children);
     console.info('Div.children[0]节点的类型', Object.prototype.toString.call(root.children[0]), root.children[0]);
@@ -16,4 +19,8 @@ window.onload = function() {
     var htmlCollection = root.children;
     console.info('htmlCollection.item: ', htmlCollection.item(0));
     console.info('htmlCollection.namedItem: ', htmlCollection.namedItem('c1'));
+
+    // HTMLCollection 没有getElementsByClassName方法
+    // getElementsByClassName 的调用者是Element的子类
+    htmlCollection.item(0).getElementsByClassName('');
 }
