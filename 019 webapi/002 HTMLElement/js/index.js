afeddf815d2824b2ff00cc04f6885aa1d760fcae
SBDavid <billl89@126.com>
2017-08-19 10:00:17 +0800

    update

--- 019 webapi/002 HTMLElement/js/index.js
+++ 019 webapi/002 HTMLElement/js/index.js
@@ -4,4 +4,19 @@ window.onload = function() {
     console.info('accessKey1.accessKey: ', accessKey1.accessKey);
 
     document.getElementById('contentEditable').contentEditable = true;
+
+    console.info('dataset', document.getElementById('dataset').dataset);
+
+    var offset =  document.getElementById('offset');
+
+    console.info('offsetHeight', offset.offsetHeight);
+    console.info('offsetWidth', offset.offsetWidth);
+    console.info('offsetTop', offset.offsetTop);
+    console.info('offsetLeft', offset.offsetLeft);
+    console.info('offsetParent', offset.offsetParent);
+
+    document.getElementById('oncopy').oncopy = function(e) {
+         e.clipboardData.setData("text", "test")
+        console.info('oncopy: ', e.clipboardData.getData("text"));
+    }
 }
