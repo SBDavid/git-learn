63179e49ce7471041afd7957673d43d959447c6c
davidtangjw <davidtangjw@pptv.com>
2017-09-11 13:51:33 +0800

    update

---
+++ 021 webpack学习/001 基础原理/src/index.js
@@ -0,0 +1,5 @@
+window.__webpack_require__ = __webpack_require__;
+if (+new Date() < 0) {
+    
+    var t = require('./ma');
+}
---
+++ 021 webpack学习/001 基础原理/src/ma.js
@@ -0,0 +1,4 @@
+window.__webpack_require__ = __webpack_require__;
+module.exports = {
+    test: 'test'
+}
