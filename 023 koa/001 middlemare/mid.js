545332bebed6c6d4aa5b2e8053dbddedb1003411
SBDavid <billl89@126.com>
2017-09-24 22:09:42 +0800

    update

--- 023 koa/001 middlemare/mid.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function logger(ctx, next) {
-    const startDate = new Date();
-    for (var index = 0; index < 1000000; index++) {
-        var element = index
-
-    }
-    next();
-    console.log(`method: ${ctx.method} code: ${ctx.status} time:${new Date() - startDate}ms`);
-}
