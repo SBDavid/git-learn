edcce2d58fa25152f61824c651405ae64ebe3340
SBDavid <billl89@126.com>
2017-09-25 22:39:07 +0800

    update

--- 022 node/002 express demo/myapp/app.js
+++ 022 node/002 express demo/myapp/app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
 	res.send('tangjiaiwe');
 });
 
-var server = app.listen(80, function() {
+app.get('/user', function (req, res) {
+	res.send('Got a PUT request at /user')
+});
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+	res.send(JSON.stringify(req.params));
+})
+
+app.use('/static', express.static('public'));
+
+var server = app.listen(80, function () {
 	console.info('started');
 });
