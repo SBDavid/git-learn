9ba5ce073ac039a0af61fba2b1246036fc9d0a39
davidtangjw <davidtangjw@pptv.com>
2017-09-29 14:05:00 +0800

    update

--- 022 node/003 express game/apps/snake/static/js/index.js
+++ 022 node/003 express game/apps/snake/static/js/index.js
@@ -1,31 +1,112 @@
+/* 坐标 */
 function coModel(x,y) {
     return {
         x: x,
         y: y
     }
 }
-
+/* 方向 */
+var moveDirection = {
+    up: 38,
+    right: 39,
+    down: 40,
+    left: 37
+};
+/* model */
 function gameModel(x,y) {
     this.x = x;
     this.y = y;
+    this.foods = [];
 }
 
 gameModel.prototype.init = function() {
-    this.head = coModel(1, 0);
-
-    this.body = [this.head, coModel(0, 0)];
+    this.body = [coModel(3, 0), coModel(2, 0), coModel(1, 0), coModel(0, 0)];
 }
 
-gameModel.prototype.moveRight = function() {
+gameModel.prototype.nextStepPosition = function(moveDir) {
+    switch (moveDir) {
+        case moveDirection.up:
+            return coModel(this.body[0].x, this.body[0].y - 1);
+            break;
+        
+        case moveDirection.right:
+            return coModel(this.body[0].x + 1, this.body[0].y);
+            break;    
+
+        case moveDirection.down:
+            return coModel(this.body[0].x, this.body[0].y + 1);
+            break;
+        
+        case moveDirection.left:
+            return coModel(this.body[0].x - 1, this.body[0].y);
+            break;       
+
+        default:
+            break;
+    }
+}
+
+gameModel.prototype.move = function(moveDir) {
     this.body.pop();
-    this.body.unshift(coModel(this.body[0].x + 1, this.body[0].y));
+    this.body.unshift(this.nextStepPosition(moveDir));
+}
+
+gameModel.prototype.eat = function(co) {
+    this.body.unshift(co);
+}
+
+gameModel.prototype.feed = function(co) {
+    this.foods.push(co);
+}
+
+gameModel.prototype.randonFeed = function() {
+    var self = this;
+    function generate() {
+        var rx = Math.floor(Math.random() * self.x);
+        var ry = Math.floor(Math.random() * self.y);
+
+        var confilct = false;
+        self.body.forEach(function(item) {
+            if (item.x === rx && item.y === ry)
+                confilct = true;
+        })
+        if (confilct) {
+            return generate();
+        } else {
+            return coModel(rx, ry);
+        }
+    }
+
+    var foodCo = generate();
+    this.feed(generate());
+    return foodCo;
+}
+
+gameModel.prototype.unfeed = function(co) {
+    var index = this.foods.indexOf(function(item){
+        return item.x === co.x && item.y === co.y;
+    })
+    if (index !== -1) {
+        this.foods.splice(index, 1);
+    }
 }
 
 function gameView(model) {
+    var self = this;
     this.gameM = model;
 
     this.rootCon = document.getElementById('root');
     this.blocks = Array.prototype.slice.call(this.rootCon.getElementsByClassName('block'), 0);
+
+    // 事件挂载
+    window.addEventListener('keyup', function(event){
+        var nextStep = self.gameM.nextStepPosition(event.keyCode);
+        if ($(self.blocks[getNum(nextStep)]).attr('food') === 'true') {
+            self.eat(nextStep);
+        } else {
+            self.moveHandler(event);
+        }
+    })
 }
 
 gameView.prototype.displayBody = function() {
@@ -33,14 +114,43 @@ gameView.prototype.displayBody = function() {
     $.each(self.gameM.body, function(index, item){
         self.show(item);
     });
+    self.showHead();
 }
 
-gameView.prototype.moveRight = function() {
-    var newHead = coModel(this.gameM.body[0].x + 1, this.gameM.body[0].y);
+gameView.prototype.move = function(moveDir) {
+    var newHead = this.gameM.nextStepPosition(moveDir);
     this.show(newHead);
     this.hide(this.gameM.body[this.gameM.body.length - 1]);
-    this.gameM.moveRight();
-    console.info(this.gameM.body)
+    this.gameM.move(moveDir);
+    this.showHead();
+}
+
+gameView.prototype.moveHandler = function(event) {
+    var self = this;
+    var head = self.gameM.body[0];
+    // 不能碰撞
+    var nextStep = self.gameM.nextStepPosition(event.keyCode);
+    var confilct = false;
+    self.gameM.body.forEach(function(item) {
+        if (nextStep.x === item.x && nextStep.y === item.y) {
+            confilct = true;
+        }
+    });
+    if (confilct) return;
+
+    if (event.keyCode === 38 && head.y > 0) {
+        console.log('上');
+        self.move(moveDirection.up);
+    } else if (event.keyCode === 39 && head.x < self.gameM.x - 1) {
+        console.log('右');
+        self.move(moveDirection.right);
+    } else if (event.keyCode === 40 && head.y < self.gameM.y - 1) {
+        console.log('下');
+        self.move(moveDirection.down);
+    } else if (event.keyCode === 37 && head.x > 0) {
+        console.log('左');
+        self.move(moveDirection.left);
+    }
 }
 
 gameView.prototype.hide = function(co) {
@@ -54,35 +164,50 @@ gameView.prototype.show = function(co) {
     $(self.blocks[getNum(co)]).addClass('show');
 }
 
-window.onload = function () {
-    
+gameView.prototype.showHead = function() {
+    var self = this;
+    $(self.blocks[getNum(self.gameM.body[0])]).addClass('head');
+    $(self.blocks[getNum(self.gameM.body[1])]).removeClass('head');
+}
 
-    // 监听键盘事件
-/*     window.addEventListener('keyup', function(event){
-        hide(currentCo);
-        if (event.keyCode === 38 && currentCo.y > 0) {
-            console.log('上');
-            currentCo.y--;
-        } else if (event.keyCode === 39 && currentCo.x < 9) {
-            console.log('右');
-            currentCo.x++;
-        } else if (event.keyCode === 40 && currentCo.y < 9) {
-            console.log('下');
-            currentCo.y++;
-        } else if (event.keyCode === 37 && currentCo.x > 0) {
-            console.log('左');
-            currentCo.x--;
-        }
-        show(currentCo);
-    }) */
+gameView.prototype.eat = function(co) {
+    var self = this;
+    self.show(co);
+    self.gameM.eat(co);
+    self.unfeed(co);
+    self.showHead();
+    self.randonFeed();
+}
+
+gameView.prototype.feed = function(co) {
+    var self = this;
+    $(self.blocks[getNum(co)]).attr('food', true);
+    $(self.blocks[getNum(co)]).addClass('food');
+    self.gameM.feed(co);
+}
+
+gameView.prototype.randonFeed = function() {
+     var foodCo = this.gameM.randonFeed();
+     this.feed(foodCo);
+}
+
+gameView.prototype.unfeed = function(co) {
+    var self = this;
+    $(self.blocks[getNum(co)]).attr('food', false);
+    $(self.blocks[getNum(co)]).removeClass('food');
+    self.gameM.unfeed(co);
+}
+
+window.onload = function () {
 
     var gameM = new gameModel(10,10);
     gameM.init();
     var gameV = new gameView(gameM);
 
     gameV.displayBody();
-    gameV.moveRight();
-    gameV.moveRight();
+
+    // feed
+    gameV.randonFeed();
 }
 
 function getNum(co) {
