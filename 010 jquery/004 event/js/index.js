c8dfae2347d2f6d44ae75d286904f48fdc056e4a
davidtangjw <davidtangjw@pptv.com>
2017-10-16 13:08:20 +0800

    update

--- 010 jquery/004 event/js/index.js
+++ 010 jquery/004 event/js/index.js
@@ -1,5 +1,16 @@
 window.onload = function() {
-    $('#root').on('click', 'a', function() {
-        console.info(11)
-    })
+    let root = $('#root');
+    document.getElementById('on').addEventListener('click', function() {
+        $(root).on('click', 'a', function(e) {
+            console.info('click', e)
+        });
+    });
+
+    document.getElementById('off').addEventListener('click', function() {
+        $(root).off('click', 'a');
+    });
+
+    document.getElementById('trigger').addEventListener('click', function() {
+        $(root).find('a').trigger('click');
+    });
 }
