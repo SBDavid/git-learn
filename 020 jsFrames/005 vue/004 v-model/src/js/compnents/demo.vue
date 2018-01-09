0176caebd2ca7ea52c3ed444efe8c07391b7abcb
SBDavid <billl89@126.com>
2018-01-09 22:43:43 +0800

    update

---
+++ 020 jsFrames/005 vue/004 v-model/src/js/compnents/demo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <input type="checkbox" v-model.lazy="toggle" />it is a input<br>
+      <label>{{toggle}}</label><br>
+      <input v-model.lazy="msg" @change="c" />
+      <label>{{msg}}</label>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({
+  props: {
+   
+  }
+})
+
+export default class demo extends Vue {
+  toggle: boolean = false;
+  msg: string = '';
+  c (e:any) {
+    console.info(e);
+  }
+}
+
+</script>
+<style></style>
