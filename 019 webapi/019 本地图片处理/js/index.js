ed2d08b02f4318dc5f090a69fd5066af051e23b4
davidtangjw <davidtangjw@pptv.com>
2018-02-14 18:21:58 +0800

    update

---
+++ 019 webapi/019 本地图片处理/js/index.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+    var showImg = document.getElementById("showImg");
+
+    var fileInput = document.getElementById('imgInput');
+
+    fileInput.click();
+    console.info('fileInput', fileInput);
+
+
+}
