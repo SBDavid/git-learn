bf3c0eb3dbf9431199196f363c5264da67bab6c1
SBDavid <billl89@126.com>
2018-03-01 21:47:51 +0800

    udpate

---
+++ 029 wxgame/2018-02-22-phaser/02_CityOfMemory/src/comps/levelPanel.js
@@ -0,0 +1,29 @@
+var game = require('../game');
+
+function levelPanel(level) {
+    this.group = new Phaser.Group(game);
+    this.group.width = 300;
+    this.group.height = 100;
+    this.group.inputEnableChildren = true;
+
+    let bg = new Phaser.Graphics(game, 0, 0);
+    bg.beginFill(0xff0000);
+    bg.drawRect(0,0,900,300);
+    bg.endFill();
+
+    this.group.add(bg);
+
+    let Level = new Phaser.Text(game, 0, 0, level+'', {
+        fontSize: '80px',
+        fontWeight: 'bold',
+        fill: '#f2bb15'
+    });
+    this.group.add(Level);
+
+    // 事件
+    this.group.onChildInputUp.add(function() {
+        console.info(level);
+    })
+}
+
+module.exports = levelPanel;
--- 029 wxgame/2018-02-22-phaser/02_CityOfMemory/src/states/choseLevel.js
+++ 029 wxgame/2018-02-22-phaser/02_CityOfMemory/src/states/choseLevel.js
@@ -1,4 +1,5 @@
-var game = require('../game');
+var game = require('../game'),
+    levelPanel = require('../comps/levelPanel');
 
 let choseLevel = function() {
 
@@ -10,6 +11,23 @@ choseLevel.prototype.preload = function() {
 }
 
 choseLevel.prototype.create = function() {
+
+    var title = game.add.text(0, 0, '选择难度', {
+        fontSize: '80px',
+        fontWeight: 'bold',
+        fill: '#f2bb15'
+    });
+    /* title.inputEnabled = true;
+
+    console.info(title.events.onInputUp.add(function() {
+        console.info(111);
+    })); */
+
+    // 增加level5
+    var level5 = new levelPanel(5).group;
+    level5.top = 100;
+    level5.left = 40;
+    game.add.world.add(level5);
 }
 
 module.exports = choseLevel;
