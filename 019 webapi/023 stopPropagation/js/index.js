1abf9550c11fbb8654d38ab4b872182e4a0829a5
SBDavid <billl89@126.com>
2018-03-16 22:59:37 +0800

    update

--- 019 webapi/023 stopPropagation/js/index.js
+++ 019 webapi/023 stopPropagation/js/index.js
@@ -1,21 +1,34 @@
 window.onload = function() {
 
     var startX = null;
+    var startLeft = null;
 
     var swiper = document.getElementById('swiper');
 
+    swiper.getElementsByTagName('ul')[0].style.transform  = 'translateX(0px)';
+
     var moveCallback = function(event) {
+        event.preventDefault();
+        // event.stopPropagation();
+        
+        
         var changeX = event.changedTouches[0].clientX - startX;
-        console.info(changeX);
+        //console.info(changeX);
+        swiper.getElementsByTagName('ul')[0].style.transform  = `translateX(${startLeft + changeX}px)`;
     }
 
     swiper.addEventListener('touchstart', function(event) {
         var startTouch = event.changedTouches[0];
         
         startX = startTouch.clientX;
+        startLeft = parseInt(swiper.getElementsByTagName('ul')[0].style.transform.slice(11, -3));
 
-        window.addEventListener('touchmove', moveCallback);
+        swiper.addEventListener('touchmove', moveCallback, {
+            passive: false
+        });
+    });
 
-
-    })
+    swiper.addEventListener('touchend', function(event) {
+        swiper.removeEventListener('touchmove', moveCallback);
+    });
 }
