22e1d23276595fdb5f6fd2d8b89577123c41edde
SBDavid <billl89@126.com>
2018-04-02 22:56:46 +0800

    update

--- 029 wxgame/2018-04-02-wxGame/game.js
+++ 029 wxgame/2018-04-02-wxGame/game.js
@@ -2,27 +2,27 @@ import './js/libs/weapp-adapter'
 import './js/libs/symbol'
 
 import Main from './js/main'
-import { canvas } from './js/libs/weapp-adapter';
+/* import { canvas } from './js/libs/weapp-adapter';
 
-// new Main()
+new Main() */
 
-var boomAudio     = new Audio()
-boomAudio.src = 'audio/offline-sound-hit.mp3';
+var boomAudio = new Audio()
+boomAudio.src = 'audio/boom.mp3';
 boomAudio.play();
 
-document.addEventListener('touchstart', function() {
+document.addEventListener('touchstart', function () {
+    console.info('touchstart');
     boomAudio.currentTime = 0
     boomAudio.play();
 });
 
-setInterval(function() {
-    boomAudio.currentTime = 0
-    boomAudio.play();
-}, 1000);
+var img = wx.createImage();
+img.src = 'images/bg.jpg';
 
-var img     = new Image()
-img.src = './images/bg.jpg';
+var ctx = canvas.getContext('2d');
 
-console.info(canvas)
+img.onload = function() {
+    console.info('onload');
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+}
 
-var ctx = canvas.getContext('2d')
