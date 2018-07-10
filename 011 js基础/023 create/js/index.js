cf5f60c23afc332ddfe9bd9ff32dd265286cb1a5
David 糖 <792723924@qq.com>
2018-07-10 18:59:13 +0800

    udpate

--- 011 js基础/023 create/js/index.js
+++ 011 js基础/023 create/js/index.js
@@ -4,7 +4,39 @@
 // 参数二：propertiesObject 则是要添加到新创建对象的可枚举属性
 
 var shape = {
-    x: 'x1'
+    x: 'x1',
+    print: function () {
+        console.info(this.x)
+    }
 };
 
+// retangle.__proto__ 指向 shape对象
 const retangle = Object.create(shape);
+
+// 使用构造方法实现继承
+function parent() {
+    this.name = 'parent';
+    console.info('parent()');
+}
+
+parent.prototype.print = function () {
+    console.info('print: ' + this.name)
+}
+
+function chlid() {
+    parent.call(this);
+}
+
+chlid.prototype = Object.create(parent.prototype, {
+    foo: {
+        enumerable: false,
+        writable: true,
+        configurable: true,
+        value: "hello"
+    }
+});
+
+chlid.prototype.constructor = chlid;
+
+var c1 = new chlid();
+
