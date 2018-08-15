77f2d11d264ecc72ce26accd972bb42ea6ebce67
SBDavid <jiawei.tang@ximalaya.com>
2018-08-15 15:07:59 +0800

    babel学习

---
+++ 033 babel学习/2018-08-15-babel-core/index.js
@@ -0,0 +1,40 @@
+const babel = require('@babel/core');
+const RN = require('babel-preset-react-native');
+const code = `
+const abc = {
+  a: function (){
+
+  },
+  b: function (){
+
+  }
+}
+`;
+
+// 插件
+const plugin = {
+    visitor: {
+        Identifier(path) {
+            if (path.node.name === 'a') {
+                // console.info('find a', path.parentPath.node);
+            }
+        }
+    },
+    // 设置了该方法的插件优先解析--- 字符串，优先加载内置的词法、语法解析器，比如jsx、ts、flow、estree
+    manipulateOptions(opts, parserOpts) {
+        console.info('manipulateOptions');
+        // parserOpts.plugins.push('typescript');
+        // parserOpts.plugins.push('jsx');
+    },
+};
+
+babel.transform(code, {
+    plugins: [plugin],
+    presets: [RN],
+}, function (err, result) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(result.code);
+    }
+});
