33453cc0bd61e71e3de38c3ed5121fe1bb1333d8
SBDavid <jiawei.tang@ximalaya.com>
2018-08-25 08:36:01 +0800

    jest测试

---
+++ 020 jsFrames/011 jest/src/sum.test.ts
@@ -0,0 +1,8 @@
+import sum from './sum';
+
+sum(1,1);
+
+/* test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+ */
---
+++ 020 jsFrames/011 jest/ts_build/src/sum.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum_1 = require("./sum");
+sum_1.default(1, 1);
+/* test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+ */ 
