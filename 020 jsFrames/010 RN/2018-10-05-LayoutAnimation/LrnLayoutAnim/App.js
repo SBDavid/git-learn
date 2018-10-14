19ee1ee4eaa07ec43359b54edc3236b3c210bd18
SBDavid <jiawei.tang@ximalaya.com>
2018-10-14 15:39:04 +0800

    chrome插件学习

--- 020 jsFrames/010 RN/2018-10-05-LayoutAnimation/LrnLayoutAnim/App.js
+++ 020 jsFrames/010 RN/2018-10-05-LayoutAnimation/LrnLayoutAnim/App.js
@@ -33,7 +33,7 @@ export default class App extends Component<AppProps> {
 
 		if (Platform.OS === 'android') {
             UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
-        }
+		}
 	}
 
 	render() {
---
+++ 020 jsFrames/013 chrome-ext/2018-10-09-dev-tool-ext/devtools.js
@@ -0,0 +1,7 @@
+chrome.devtools.panels.create("My Panel",
+    "MyPanelIcon.png",
+    "panel.html",
+    function (panel) {
+        // code invoked on panel creation
+    }
+);
