841711b74e14a22215773dbbff8ef41316a80dab
SBDavid <jiawei.tang@ximalaya.com>
2018-12-09 16:02:32 +0800

    rn-ts-template

---
+++ 020 jsFrames/010 RN/template-ts-0.52/App.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+
+export default class App extends Component {
+
+	render() {
+		return (
+			<View style={styles.container}>
+				
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		backgroundColor: 'powderblue',
+	},
+});
